717f827fc5e9b4be098814e8cf4707c64a3b3e5e
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 18:45:48 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'yjIJIYQO8xM8pEQawixA4TY457vqyF9t';//'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
