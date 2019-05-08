5270af13e4fa469c473bfc7d71036dd159d57af6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 17:31:43 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -9,7 +9,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 // Code LatestMovieReviewsContainer Here
 export default class LatestMovieReviewsContainer extends Component {
   state = {
-    movies: []
+    reviews: []
   }
 
 }
