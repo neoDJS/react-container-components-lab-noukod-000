93ece0314465c31c996a8b0e40fed904ecb95141
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 17:46:05 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews})=>{
+  return(
+    <div class="review-list">
+      {reviews.map(r=><div class="review"></div>)}
+    </div>
+  )
+}
+
+export default MovieReviews
